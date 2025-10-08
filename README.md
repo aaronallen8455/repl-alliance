2ba04be2ba5f6bc5000d051567499c0d46f25d90
Aaron Allen <aaronallen8455@gmail.com>
2025-10-07 20:45:10 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# repl-alliance
+
+A collection of plugins that enhance a REPL based development workflow.
---
+++ src/ReplAlliance.hs
@@ -0,0 +1,18 @@
+module ReplAlliance
+  ( plugin
+  ) where
+
+import qualified PinnedWarnings
+import qualified AutoExtract
+import qualified AutoSplit
+import qualified AutoImport
+import           GHC.Plugins
+import           MonoidalPlugins
+
+plugin :: Plugin
+plugin = foldPlugins
+  [ PinnedWarnings.plugin
+  , AutoSplit.plugin
+  , AutoImport.plugin
+  , AutoExtract.plugin
+  ]
